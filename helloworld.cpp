9598e7e2bc1fb8e8ff12885488fed5fe37d93298
Rakesh1696 <145083889+Rakesh1696@users.noreply.github.com>
2024-10-30 13:38:55 -0500

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  Rakeshcode
+//
+//  Created by Rakesh Nerella on 9/30/24.
+//
+
+#include<iostream>
+
+int main(int argc, const char * argv[]) {
+   std::cout << "Hello, World!\n";
+    int favorite_number;
+    std::cout<<"Enter your favorite number between 1 and 100:";
+    std::cin>>favorite_number;
+    std::cout<<"Amazing!!that's my favorite number too! \n"<<favorite_number<<std::endl ;
+      return 0;
+}
